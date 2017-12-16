d7a8716e43bfd7e6cb0aa8d77d4d5b5e8a45c2cd
Pedro Gabriel <PeteGabriel@users.noreply.github.com>
2017-12-16 14:01:21 +0000

    Update StarRatingComponent.js

--- components/StarRatingComponent.js
+++ components/StarRatingComponent.js
@@ -1,44 +1,6 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
-/*
-The StarRating component requires two critical pieces of data: the total number of stars to display, and the rating (the number of stars to highlight).
-
-class StarRatingComponent extends Component{
-
-  static defaultProps = {
-    totalStars : 5
-  }
-
-  constructor(props){
-    super(props)
-    this.change = this.change.bind(this)
-  }
-
-  change(starsSelected){
-    this.props.onRatingChange(starsSelected)
-  }
-
-  render(){
-    const totalStars = this.props.totalStars
-    const starsSelected = this.props.rating
-
-    return (
-      <div>
-        {Array.from(new Array(totalStars), (n, i) =>
-          <Star key={i} selected={i<starsSelected} onClick={() => this.change(i+1)}/> )}  
-        <br/>
-        <p>{starsSelected} of {totalStars} stars</p>
-      </div>
-    )
-  }
-
-}
-
-StarRatingComponent.propTypes = {
-  totalStars : PropTypes.number
-}
-*/
 const StarRatingComponent= ({rating=0, onRatingChange, totalStars=5}) => 
   <div>
     {Array.from(new Array(totalStars), (n, i) =>
